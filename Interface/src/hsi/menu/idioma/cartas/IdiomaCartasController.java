85a1af0ad56c1555674690d4fb1d4b81921b29aa
Cristo Manuel Estévez hernández <19151793+Cristoto@users.noreply.github.com>
2018-02-07 15:23:47 +0000

    Agregación opciones idioma
    Agregación de las opciones de idiomas del menú.

---
+++ Interface/src/hsi/menu/idioma/cartas/IdiomaCartasController.java
@@ -0,0 +1,92 @@
+package hsi.menu.idioma.cartas;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import hsi.app.HsiApp;
+import hsi.unirest.herramientas.ServicioAPI;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class IdiomaCartasController implements Initializable {
+
+	private Stage stage;
+	
+	//Model
+	private ListProperty<String> idiomas;
+	private StringProperty idiomaSelecionado;
+	
+	// View
+	@FXML
+	private GridPane view;
+
+	@FXML
+	private ComboBox<String> idiomasComboBox;
+
+	@FXML
+	private Button aceptarBtn;
+
+	@FXML
+	private Button cancelarBtn;
+
+	public IdiomaCartasController() throws IOException {
+		idiomas = new SimpleListProperty<>(this, "idiomas", FXCollections.observableArrayList());
+		idiomaSelecionado = new SimpleStringProperty(this, "idiomaSeleccionados");
+		
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("IdiomaCartasView.fxml"));
+		loader.setController(this);
+		loader.load();
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		//BINDEOS
+		idiomasComboBox.itemsProperty().bind(idiomas);
+		idiomaSelecionado.bind(idiomasComboBox.getSelectionModel().selectedItemProperty());
+		
+		idiomasComboBox.getSelectionModel().select("esES");
+		
+		//Eventos
+		cancelarBtn.setOnAction(e -> onCancelarBtnAction(e));
+		aceptarBtn.setOnAction(e -> onAceptarBtnAction(e));
+	}
+	
+	private void onAceptarBtnAction(ActionEvent e) {
+		stage.close();
+	}
+
+	private void onCancelarBtnAction(ActionEvent e) {
+		stage.close();
+	}
+
+	public String crearVentana() {
+		stage = new Stage();
+		stage.initOwner(HsiApp.getPrimaryStage());
+		//stage.getIcons().add(HsiApp.getPrimaryStage().getIcons().get(0));
+		stage.initModality(Modality.APPLICATION_MODAL);
+		stage.setTitle("Cambiar Idioma de las Cartas");
+		stage.setScene(new Scene(view));
+		stage.setResizable(false);
+		stage.showAndWait();
+		return idiomaSelecionado.get();
+	}
+	
+	public ListProperty<String> getIdiomas() {
+		return idiomas;
+	}
+
+}
